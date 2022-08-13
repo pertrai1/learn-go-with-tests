ed948ecb6e99e994db1d508455177dd9b11b35e2
Rob Simpson <rsimpson2@me.com>
2022-08-13 16:52:04 -0400

    sum using for loop

---
+++ sum/sum.go
@@ -0,0 +1,26 @@
+package main
+
+func Sum(nums []int) int {
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
